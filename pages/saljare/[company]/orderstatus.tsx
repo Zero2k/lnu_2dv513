7a26fd92b716b3a7203db10c6cd8c6b35a97c9d1
Viktor Bengtsson <vibe16@student.bth.se>
2021-01-25 11:31:08 +0100

    Updated saljare/company and replaced saljare/produkter with saljare/orderstatus

--- pages/saljare/[company]/orderstatus.tsx
+++ pages/saljare/[company]/orderstatus.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useStyletron } from 'baseui';
+
 import Section from 'components/Section';
 import Breadcrumb from 'components/Breadcrumb';
 import { Grid, Cell } from 'baseui/layout-grid';
@@ -7,42 +8,15 @@ import { ListItem, ListItemLabel } from 'baseui/list';
 import { Heading, HeadingLevel } from 'baseui/heading';
 import { useQuery } from '@apollo/client';
 import { FIND_USER_QUERY } from 'graphql/user';
-import Product from 'components/Product';
-import Cart from 'components/Cart';
-import OrderForm from 'forms/orderForm';
-import Link from 'next/link';
 
-const Company: React.FC = () => {
+export const sum = (a: number, b: number) => a + b;
+
+const OrderStatus: React.FC = () => {
   const [css, theme] = useStyletron();
   const [display, setDisplay] = React.useState(false);
-  const [formPart, setFormPart] = React.useState(1);
-  const [cart, setCart] = React.useState([]);
-
   const { data, loading } = useQuery(FIND_USER_QUERY, {
     variables: { userId: 1 },
   });
-
-  const addToCart = (product) => {
-    const tempProduct = {
-      id: product.id,
-      quantity: 1,
-      price: product.price,
-      name: product.name,
-      url: product.name,
-    };
-    const productExists = cart.some((item) => item.id === product.id);
-    if (!productExists) {
-      const newProduct = [tempProduct, ...cart];
-      setCart(newProduct);
-    }
-  };
-
-  const removeFromCart = (product) => {
-    const newCart = cart.filter((p) => {
-      return p.id != product.id;
-    });
-    setCart(newCart);
-  };
 
   if (loading) {
     return (
@@ -141,59 +115,8 @@ const Company: React.FC = () => {
           </Cell>
           <Cell span={[12, 8]}>
             <HeadingLevel>
-              <Heading styleLevel={5}>
-                Varukorg ({cart.length}) & Beställning{' '}
-                {formPart === 2 ? (
-                  <small
-                    className={css({
-                      fontSize: '12px',
-                      textDecoration: 'underline',
-                      cursor: 'pointer',
-                    })}
-                    onClick={() => setFormPart(1)}
-                  >
-                    Ändra varukorg
-                  </small>
-                ) : null}
-              </Heading>
+              <Heading styleLevel={5}>Orderstatus</Heading>
             </HeadingLevel>
-            {formPart === 1 ? (
-              <>
-                <Cart
-                  products={cart}
-                  removeFromCart={removeFromCart}
-                  setFormPart={setFormPart}
-                />
-                <p>
-                  Har du beställt? Se orderstatus{' '}
-                  <Link href={`/saljare/${company.id}/orderstatus`} passHref>
-                    <a>här</a>
-                  </Link>
-                </p>
-              </>
-            ) : formPart === 2 ? (
-              <OrderForm resellerId={company.id} cart={cart} />
-            ) : null}
-          </Cell>
-          <Cell span={[12]}>
-            <HeadingLevel>
-              <Heading styleLevel={5}>Produkter</Heading>
-            </HeadingLevel>
-            <Grid
-              gridMargins={0}
-              gridColumns={12}
-              gridGaps={[3, 6, 12]}
-              gridGutters={[3, 6, 12]}
-            >
-              {company.products.map((product) => (
-                <Cell span={[12, 4]} key={product.id}>
-                  <Product
-                    product={product}
-                    addCart={() => addToCart(product)}
-                  />
-                </Cell>
-              ))}
-            </Grid>
           </Cell>
         </Grid>
       </Section>
@@ -201,4 +124,4 @@ const Company: React.FC = () => {
   );
 };
 
-export default Company;
+export default OrderStatus;
